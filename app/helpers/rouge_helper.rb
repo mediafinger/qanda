467afb4487699d2477e2966f8d5feb7b92d6b48a
Andreas Finger <webmaster@mediafinger.com>
2018-05-21 11:17:59 +0200

    Fix quoting to keep it consistent

--- app/helpers/rouge_helper.rb
+++ app/helpers/rouge_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module RougeHelper
-  require 'redcarpet'
-  require 'rouge'
-  require 'rouge/plugins/redcarpet'
+  require "redcarpet"
+  require "rouge"
+  require "rouge/plugins/redcarpet"
 
   class HTMLRenderWithCodeHighlighting < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
