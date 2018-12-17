3499d996064f03771ef7e98e461a6a7f756c8aa8
Andreas Finger <webmaster@mediafinger.com>
2018-12-17 17:34:46 +0100

    Update Capybara and FactoryBot rubocop cops

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -2,31 +2,33 @@
 
 FactoryBot.define do
   factory :question do
-    title "Why?"
-    body  "Why did the lucky stiff disappear?"
+    title { "Why?" }
+    body { "Why did the lucky stiff disappear?" }
     user
 
     trait :markdown do
-      title "Did my markdown disappear?"
-      body  <<~MARKDOWN
-        # Headline
+      title { "Did my markdown disappear?" }
+      body do
+        <<~MARKDOWN
+          # Headline
 
-        Some text in _italic_ and **bold**
+          Some text in _italic_ and **bold**
 
-        * bullet
-        * points
+          * bullet
+          * points
 
-        ## Lucky part
+          ## Lucky part
 
-        A plain link: https://localhost:3000
+          A plain link: https://localhost:3000
 
-        ---
+          ---
 
-        Line breaks
-        Should be ignored
+          Line breaks
+          Should be ignored
 
-        Unless there is a blank line in between
-      MARKDOWN
+          Unless there is a blank line in between
+        MARKDOWN
+      end
     end
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "harry-#{n}@example.com" }
-    name "Harry"
-    provider "google"
+    name { "Harry" }
+    provider { "google" }
     sequence(:provider_uid, 1000) { |n| n }
   end
 end
